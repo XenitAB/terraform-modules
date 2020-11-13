492f3366156f2aa98df50ce243d5dfc9806b1d21
Philip Laine <philip.laine@xenit.se>
2020-11-13 15:47:08 +0100

    Add helm and kubernetes provider

--- modules/azure/aks/main.tf
+++ modules/azure/aks/main.tf
@@ -25,6 +25,24 @@ terraform {
   }
 }
 
+provider "kubernetes" {
+  load_config_file       = "false"
+  host                   = azurerm_kubernetes_cluster.this.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.this.kube_admin_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.this.kube_admin_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_admin_config.0.cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes {
+    load_config_file       = "false"
+    host                   = azurerm_kubernetes_cluster.this.kube_config.0.host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.this.kube_admin_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.this.kube_admin_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_admin_config.0.cluster_ca_certificate)
+  }
+}
+
 data "azurerm_resource_group" "this" {
   name = "rg-${var.environment}-${var.location_short}-${var.name}"
 }

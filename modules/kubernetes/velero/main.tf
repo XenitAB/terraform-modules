0c2cfcbcd029e8e13915e4502114456ad6afeecd
Simon Gottschlag <simon.gottschlag@xenit.se>
2020-11-18 16:47:00 +0100

    Add velero

---
+++ modules/kubernetes/velero/main.tf
@@ -0,0 +1,69 @@
+terraform {
+  required_version = "0.13.5"
+
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "1.13.3"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "1.3.2"
+    }
+  }
+}
+
+locals {
+  values = templatefile("${path.module}/templates/values.yaml.tpl", { cloud_provider = var.cloud_provider, azure_resource_group = var.azure_resource_group, azure_storage_account_name = var.azure_storage_account_name, azure_storage_account_container = var.azure_storage_account_container })
+}
+
+resource "kubernetes_namespace" "velero" {
+  metadata {
+    labels = {
+      name = "velero"
+    }
+    name = "velero"
+  }
+}
+
+resource "kubernetes_secret" "velero" {
+  metadata {
+    name      = "velero"
+    namespace = kubernetes_namespace.velero.metadata[0].name
+  }
+
+  data = {
+    "cloud" = yamlencode({
+      AZURE_SUBSCRIPTION_ID = var.azure_subscription_id
+      AZURE_RESOURCE_GROUP  = var.azure_resource_group
+      AZURE_CLOUD_NAME      = "AzurePublicCloud"
+    })
+  }
+}
+
+resource "helm_release" "velero" {
+  repository = "https://vmware-tanzu.github.io/helm-charts"
+  chart      = "velero"
+  name       = "velero"
+  namespace  = kubernetes_namespace.velero.metadata[0].name
+  version    = "2.13.7"
+  values     = [local.values]
+}
+
+resource "helm_release" "velero_extras" {
+  depends_on = [helm_release.velero]
+
+  chart     = "${path.module}/charts/velero-extras"
+  name      = "velero-extras"
+  namespace = kubernetes_namespace.velero.metadata[0].name
+
+  set {
+    name  = "resourceID"
+    value = var.azure_resource_id
+  }
+
+  set {
+    name  = "clientID"
+    value = var.azure_client_id
+  }
+}
---
+++ modules/kubernetes/velero/templates/values.yaml.tpl
@@ -0,0 +1,22 @@
+%{ if cloud_provider == "azure" }
+configuration:
+  provider: "azure"
+  backupStorageLocation:
+    name: "default"
+    bucket: "${azure_storage_account_container}"
+    config:
+      resourceGroup: "${azure_resource_group}"
+      storageAccount: "${azure_storage_account_name}"
+
+snapshotsEnable: false 
+
+initContainers:
+  - name: "velero-plugin-for-microsoft-azure"
+    image: "velero/velero-plugin-for-microsoft-azure:1.1.1"
+    volumeMounts:
+      - mountPath: "/target"
+        name: "plugins"
+
+podLabels:
+  aadpodidbinding: velero
+%{ endif }

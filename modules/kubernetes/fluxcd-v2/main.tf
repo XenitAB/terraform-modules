4ccfad7ddf81510306c5e1040203922b84663bd7
Philip Laine <philip.laine@xenit.se>
2020-11-18 20:21:12 +0100

    Change version constraints

--- modules/kubernetes/fluxcd-v2/main.tf
+++ modules/kubernetes/fluxcd-v2/main.tf
@@ -4,14 +4,14 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 1.13.3"
+      version = "1.13.3"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = ">= 1.9.1"
+      version = "1.9.1"
     }
     flux = {
-      source  = "fluxcd/flux"
+      source = "fluxcd/flux"
       version = "0.0.3"
     }
   }

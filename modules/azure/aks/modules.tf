ebb9810a2fe052eeab3a83552e6725492b61e217
Philip Laine <philip.laine@xenit.se>
2020-11-22 22:47:29 +0100

    Exclude flux-system

--- modules/azure/aks/modules.tf
+++ modules/azure/aks/modules.tf
@@ -146,7 +146,7 @@ module "opa_gatekeeper" {
 
   exclude = [
     {
-      excluded_namespaces = ["kube-system", "gatekeeper-system", "aad-pod-identity", "cert-manager", "ingress-nginx", "velero", "azdo-proxy"]
+      excluded_namespaces = ["kube-system", "gatekeeper-system", "aad-pod-identity", "cert-manager", "ingress-nginx", "velero", "azdo-proxy", "flux-system"]
       processes           = ["*"]
     }
   ]

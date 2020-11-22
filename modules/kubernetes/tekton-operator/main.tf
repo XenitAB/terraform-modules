330379c79d3ce9584e302dfae33776a43a5be9bf
Simon Gottschlag <simon.gottschlag@xenit.se>
2020-11-22 14:54:07 +0100

    Fix file path

--- modules/kubernetes/tekton-operator/main.tf
+++ modules/kubernetes/tekton-operator/main.tf
@@ -43,6 +43,6 @@ resource "helm_release" "tekton_operator" {
 resource "helm_release" "tekton_operator_extras" {
   depends_on = [helm_release.tekton_operator]
   name       = local.helm_release_name
-  chart      = "${path.module}/charts/tekton-operator_extras"
+  chart      = "${path.module}/charts/tekton-operator-extras"
   namespace  = kubernetes_namespace.tekton_operator.metadata[0].name
 }

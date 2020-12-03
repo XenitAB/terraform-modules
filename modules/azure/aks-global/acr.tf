ad6c7cba06bb4cc3652cff87b3c019a4fb43b718
Simon Gottschlag <simon.gottschlag@xenit.se>
2020-12-03 10:39:08 +0100

    Update group names

--- modules/azure/aks-global/acr.tf
+++ modules/azure/aks-global/acr.tf
@@ -18,12 +18,12 @@ resource "azurerm_role_assignment" "aks" {
 
 # Add data source for the Azure AD Group for AcrPull
 data "azuread_group" "acr_pull" {
-  name = "aks-${var.subscription_name}-${var.environment}-acrpull"
+  name = "${var.aks_group_name_prefix}${var.group_name_separator}${var.subscription_name}${var.group_name_separator}${var.environment}${var.group_name_separator}acrpull"
 }
 
 # Add data source for the Azure AD Group for AcrPush
 data "azuread_group" "acr_push" {
-  name = "aks-${var.subscription_name}-${var.environment}-acrpush"
+  name = "${var.aks_group_name_prefix}${var.group_name_separator}${var.subscription_name}${var.group_name_separator}${var.environment}${var.group_name_separator}acrpush"
 }
 
 # Assign AcrPull permissions to the Azure AD Group for AcrPull

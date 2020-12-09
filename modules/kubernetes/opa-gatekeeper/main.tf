ee6ec423c6eb5c4c96412bede0637a921b023369
Philip Laine <philip.laine@gmail.com>
2020-12-09 15:05:42 +0100

    Update gatekeeper versions (#77)
    * Update gatekeeper charts and add external ips constraint
    
    * Update gatekeeper chart version
    
    * Run generate

--- modules/kubernetes/opa-gatekeeper/main.tf
+++ modules/kubernetes/opa-gatekeeper/main.tf
@@ -96,8 +96,8 @@ terraform {
 }
 
 locals {
-  gatekeeper_version         = "v3.2.1"
-  gatekeeper_library_version = "v0.4.3"
+  gatekeeper_version         = "v3.2.2"
+  gatekeeper_library_version = "v0.5.1"
   values                     = templatefile("${path.module}/templates/gatekeeper-library-values.yaml.tpl", { constraints = concat(var.default_constraints, var.additional_constraints), exclude = var.exclude })
 }
 

053820c4fd128e2a5471a0f3a1cb63c330a63d45
Philip Laine <philip.laine@xenit.se>
2020-12-10 20:44:59 +0100

    Fix resource names

--- modules/aws/core/vpc.tf
+++ modules/aws/core/vpc.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "this" {
 }
 
 resource "aws_internet_gateway" "this" {
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.this.id
 
   tags = {
     Name = var.name
@@ -19,7 +19,7 @@ resource "aws_internet_gateway" "this" {
 }
 
 resource "aws_route_table" "this" {
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.this.id
 
   tags = {
     Name = "${var.name}-default"
@@ -28,9 +28,9 @@ resource "aws_route_table" "this" {
 }
 
 resource "aws_route" "internet" {
-  route_table_id         = aws_route_table.rtDefault.id
+  route_table_id         = aws_route_table.this.id
+  gateway_id             = aws_internet_gateway.this.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
 }
 
 #resource "aws_subnet" "this" {

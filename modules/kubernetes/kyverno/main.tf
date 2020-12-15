c8d1ec86cd927660bf960f8f4400a13024ad0c7c
Philip Laine <philip.laine@xenit.se>
2020-12-15 16:28:27 +0100

    Run generate

--- modules/kubernetes/kyverno/main.tf
+++ modules/kubernetes/kyverno/main.tf
@@ -46,5 +46,5 @@ resource "helm_release" "kyverno_extras" {
   chart     = "${path.module}/charts/kyverno-extras"
   name      = "kyverno-extras"
   namespace = kubernetes_namespace.this.metadata[0].name
-  values = [templatefile("${path.module}/templates/values.yaml.tpl", { excluded_namespaces = var.excluded_namespaces })]
+  values    = [templatefile("${path.module}/templates/values.yaml.tpl", { excluded_namespaces = var.excluded_namespaces })]
 }

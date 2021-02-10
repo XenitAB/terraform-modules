d1c376d4442234ec6a3e3aab559ff7abf00e5d68
Tobias Vuorenmaa <tobias.vuorenmaa@azr-lab.net>
2021-02-10 23:00:24 +0100

    Added wvd resources and variables

--- modules/azure/wvd/main.tf
+++ modules/azure/wvd/main.tf
@@ -19,4 +19,22 @@ data "azurerm_resource_group" "this" {
   name = "rg-${var.environment}-${var.location_short}-${var.name}"
 }
 
+resource "azurerm_virtual_desktop_host_pool" "this" {
+  name                = "pool-${var.environment}-${var.location_short}-${var.name}${var.wvd_name_suffix}"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
 
+  type               = var.wvd_config.pool_type
+  load_balancer_type = var.wvd_config.load_balancer_type
+}
+
+resource "azurerm_virtual_desktop_application_group" "this" {
+  name                = "acctag"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  type          = "RemoteApp"
+  host_pool_id  = azurerm_virtual_desktop_host_pool.this.id
+  friendly_name = "TestAppGroup"
+  description   = "Acceptance Test: An application group"
+}
--- modules/azure/wvd/variables.tf
+++ modules/azure/wvd/variables.tf
@@ -17,3 +17,26 @@ variable "core_name" {
   description = "The commonName for the core infrastructure"
   type        = string
 }
+
+variable "wvd_name_suffix" {
+  description = "The suffix for the wvd host pool"
+  type        = number
+  default     = 1
+}
+
+variable "wvd_config" {
+  description = "The Windows Virtual Dekstop (WVD) configuration"
+  type = object({
+    pool_type = string
+    load_balancer_type = string
+    maximum_allowed_sessions = number
+    application_group_type = string
+    default_node_pool = object({
+      orchestrator_version = string
+      vm_size              = string
+      min_count            = number
+      max_count            = number
+      node_labels          = map(string)
+    })
+  })
+}

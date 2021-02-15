6927e0eb71f14afb511fd7cde1b9ff4b1d321311
Tobias Vuorenmaa <tobias.vuorenmaa@azr-lab.net>
2021-02-15 12:22:16 +0100

    Removed Regions from module

--- modules/azure/bastion/main.tf
+++ modules/azure/bastion/main.tf
@@ -24,51 +24,32 @@ terraform {
 }
 
 data "azurerm_resource_group" "this" {
-  for_each = {
-    for env_resource in local.env_resources :
-    env_resource.name => env_resource
-  }
-
-  name = "rg-${each.value.name}"
+  name = "rg-${var.environment}-${var.location_short}-${var.name}"
 }
 
 resource "azurerm_public_ip" "this" {
-  for_each = {
-    for subnet in local.bastions :
-    subnet.subnet_name => subnet
-  }
-
-  name                = "pip-${each.value.vnet_resource}-bastion"
-  location            = data.azurerm_resource_group.this[each.value.vnet_resource].location
-  resource_group_name = data.azurerm_resource_group.this[each.value.vnet_resource].name
+  name                = "pip-${var.environment}-${var.location_short}-${var.name}-bastion"
+  location            = data.azurerm_resource_group.this.location
+  resource_group_name = data.azurerm_resource_group.this.name
   sku                 = "Standard"
   allocation_method   = "Static"
 }
 
 resource "azurerm_subnet" "this" {
-  for_each = {
-    for subnet in local.bastions :
-    subnet.subnet_name => subnet
-  }
-
-  name                 = each.value.subnet_name
-  resource_group_name  = data.azurerm_resource_group.this[each.value.vnet_resource].name
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = data.azurerm_resource_group.this.name
   virtual_network_name = var.vnet_name
-  address_prefixes     = [each.value.subnet_cidr]
+  address_prefixes     = var.bastion_subnet_config.cidr
 }
 
 resource "azurerm_bastion_host" "this" {
-  for_each = {
-    for subnet in local.bastions :
-    subnet.subnet_name => subnet
-  }
-  name                = "bastion-${each.value.vnet_resource}"
-  location            = data.azurerm_resource_group.this[each.value.vnet_resource].location
-  resource_group_name = data.azurerm_resource_group.this[each.value.vnet_resource].name
+  name                = "bastion-${var.environment}-${var.location_short}-${var.name}"
+  location            = data.azurerm_resource_group.this.location
+  resource_group_name = data.azurerm_resource_group.this.name
 
   ip_configuration {
     name                 = "configuration"
-    subnet_id            = azurerm_subnet.this[each.key].id
-    public_ip_address_id = azurerm_public_ip.this[each.key].id
+    subnet_id            = azurerm_subnet.this.id
+    public_ip_address_id = azurerm_public_ip.this.id
   }
 }
---
+++ modules/azure/wvd/main.tf
@@ -0,0 +1,40 @@
+/**
+  * # Windows Virtual Desktop
+  *
+  * This module is used to create Windows Virtual Desktop.
+  */
+
+terraform {
+  required_version = "0.13.5"
+
+  required_providers {
+    azurerm = {
+      version = "2.47.0"
+      source  = "hashicorp/azurerm"
+    }
+  }
+}
+
+data "azurerm_resource_group" "this" {
+  name = "rg-${var.environment}-${var.location_short}-${var.name}"
+}
+
+resource "azurerm_virtual_desktop_host_pool" "this" {
+  name                = "pool-${var.environment}-${var.location_short}-${var.name}${var.wvd_name_suffix}"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  type               = var.wvd_config.pool_type
+  load_balancer_type = var.wvd_config.load_balancer_type
+}
+
+resource "azurerm_virtual_desktop_application_group" "this" {
+  name                = "ag-${var.environment}-${var.location_short}-${var.name}${var.wvd_name_suffix}"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  type          = "RemoteApp"
+  host_pool_id  = azurerm_virtual_desktop_host_pool.this.id
+  friendly_name = "TestAppGroup"
+  description   = "Acceptance Test: An application group"
+}

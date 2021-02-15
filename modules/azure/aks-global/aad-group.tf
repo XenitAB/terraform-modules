55f45ee1dba370dc6e46b3e6c445c80387524f41
Philip Laine <philip.laine@xenit.se>
2021-02-15 15:46:26 +0100

    Run format

--- modules/azure/aks-global/aad-group.tf
+++ modules/azure/aks-global/aad-group.tf
@@ -1,13 +1,13 @@
 resource "azuread_group" "view" {
-  for_each = { for ns in var.namespaces : ns.name => ns }
-  display_name     = "${var.aks_group_name_prefix}${var.group_name_separator}${var.subscription_name}${var.group_name_separator}${var.environment}${var.group_name_separator}${each.value.name}${var.group_name_separator}view"
+  for_each     = { for ns in var.namespaces : ns.name => ns }
+  display_name = "${var.aks_group_name_prefix}${var.group_name_separator}${var.subscription_name}${var.group_name_separator}${var.environment}${var.group_name_separator}${each.value.name}${var.group_name_separator}view"
   #description             = "Members of this group will have view access to the ${each.value.name} namespace."
   prevent_duplicate_names = true
 }
 
 resource "azuread_group" "edit" {
-  for_each = { for ns in var.namespaces : ns.name => ns }
-  display_name     = "${var.aks_group_name_prefix}${var.group_name_separator}${var.subscription_name}${var.group_name_separator}${var.environment}${var.group_name_separator}${each.value.name}${var.group_name_separator}edit"
+  for_each     = { for ns in var.namespaces : ns.name => ns }
+  display_name = "${var.aks_group_name_prefix}${var.group_name_separator}${var.subscription_name}${var.group_name_separator}${var.environment}${var.group_name_separator}${each.value.name}${var.group_name_separator}edit"
   #description             = "Members of this group will have edit access to the ${each.value.name} namespace."
   prevent_duplicate_names = true
 }

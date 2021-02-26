b132b48a5b7d6e7851f7509aa5a7ba7d9af31b41
Simon Gottschlag <simon.gottschlag@xenit.se>
2021-02-26 10:41:02 +0100

    Add resource lock to rg

--- modules/azure/governance-regional/rg.tf
+++ modules/azure/governance-regional/rg.tf
@@ -15,3 +15,16 @@ resource "azurerm_resource_group" "rg" {
     each.value.tags
   )
 }
+
+resource "azurerm_management_lock" "rg" {
+  for_each = {
+    for rg in var.resource_group_configs :
+    rg.common_name => rg
+    if rg.lock_resource_group
+  }
+
+  name       = "DoNotDelete"
+  scope      = azurerm_resource_group.rg[each.key].id
+  lock_level = "CanNotDelete"
+  notes      = "This Resource Group can't be deleted without first removing the lock."
+}

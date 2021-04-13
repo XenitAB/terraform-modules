3520131d10a67942463d00ed49fe441b6105f0b0
Edvin Norling <edvin.norling@gmail.com>
2021-04-13 16:45:18 +0200

    fix fmt

--- modules/kubernetes/prometheus/main.tf
+++ modules/kubernetes/prometheus/main.tf
@@ -46,9 +46,9 @@ resource "helm_release" "prometheus" {
 resource "helm_release" "prometheus_extras" {
   depends_on = [helm_release.prometheus]
 
-  chart      = "${path.module}/charts/prometheus-extras"
-  name       = "prometheus-extras"
-  namespace  = kubernetes_namespace.this.metadata[0].name
+  chart     = "${path.module}/charts/prometheus-extras"
+  name      = "prometheus-extras"
+  namespace = kubernetes_namespace.this.metadata[0].name
   values = [templatefile("${path.module}/templates/values-extras.yaml.tpl", {
     remote_write_enabled   = var.remote_write_enabled
     remote_write_url       = var.remote_write_url

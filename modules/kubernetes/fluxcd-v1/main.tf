38634167488ed7d0a1f6f8f6ac1da367873dab31
Simon Gottschlag <simon.gottschlag@xenit.se>
2021-04-14 16:14:28 +0200

    Revert version

--- modules/kubernetes/fluxcd-v1/main.tf
+++ modules/kubernetes/fluxcd-v1/main.tf
@@ -113,7 +113,7 @@ resource "kubernetes_secret" "azdo_proxy" {
 resource "helm_release" "azdo_proxy" {
   repository = "https://xenitab.github.io/azdo-proxy/"
   chart      = "azdo-proxy"
-  version    = "v0.3.6"
+  version    = "v0.3.2"
   name       = kubernetes_namespace.azdo_proxy.metadata[0].name
   namespace  = kubernetes_namespace.azdo_proxy.metadata[0].name
 

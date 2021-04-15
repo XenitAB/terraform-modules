834e58ef0ff6d5468e960631a4654134587e7c4f
Philip Laine <philip.laine@xenit.se>
2021-04-15 13:45:53 +0200

    Add ingress name override and internal lb setting

--- modules/kubernetes/ingress-nginx/main.tf
+++ modules/kubernetes/ingress-nginx/main.tf
@@ -37,5 +37,9 @@ resource "helm_release" "ingress_nginx" {
   version    = "3.29.0"
   values = [templatefile("${path.module}/templates/values.yaml.tpl", {
     http_snippet = var.http_snippet,
+    name_override = var.name_override != "" ? var.name_override : null
+    provider = var.cloud_provider
+    ingress_class = join(",", compact(["nginx", var.name_override]))
+    internal_load_balancer = var.internal_load_balancer
   })]
 }
--- modules/kubernetes/ingress-nginx/templates/values.yaml.tpl
+++ modules/kubernetes/ingress-nginx/templates/values.yaml.tpl
@@ -8,3 +8,12 @@ controller:
     http-snippet: |
       ${http_snippet}
     %{ endif }
+
+nameOverride: ${name_override}
+ingressClass: ${ingress_class}
+
+%{ if provider == "aws" && internal_load_balancer }
+service:
+  annotations:
+    service.beta.kubernetes.io/aws-load-balancer-internal
+%{ endif }

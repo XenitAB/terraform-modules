47fd483e3ec79c07320b5f286cf520406b38c214
Simon Gottschlag <simon.gottschlag@xenit.se>
2021-05-24 00:44:41 +0200

    ...

--- validation/azure/aks-power-schedule/main.tf
+++ validation/azure/aks-power-schedule/main.tf
@@ -9,10 +9,10 @@ module "aks_power_schedule" {
 
   environment                  = "dev"
   location_short               = "we"
-  unique_suffix                = "9876"
+  unique_suffix                = "123"
   name                         = "aks"
   resource_group_name          = "fnlab"
   aks_cluster_name             = "fnlab"
-  shutdown_aks_cron_expression = "*/5 * * * * *"
+  shutdown_aks_cron_expression = "* 0 2 * * *"
   shutdown_aks_disabled        = false
 }

efff1e997f9ea46755b5e91e0a21d79b24b3ebed
Edvin Norling <edvin.norling@xenit.se>
2021-07-07 15:08:01 +0200

    Remove aws_iam_role_policy_attachment from global
    Use managed_policy_arns in aws_iam_roles instead.
    This way we don't have to think about dependecies.

--- modules/aws/eks-global/iam.tf
+++ modules/aws/eks-global/iam.tf
@@ -71,11 +71,9 @@ resource "aws_iam_policy" "eks_admin" {
 resource "aws_iam_role" "eks_admin" {
   name               = "eks-admin"
   assume_role_policy = data.aws_iam_policy_document.eks_admin_assume.json
-}
-
-resource "aws_iam_role_policy_attachment" "eks_admin" {
-  role       = aws_iam_role.eks_admin.name
-  policy_arn = aws_iam_policy.eks_admin.arn
+  managed_policy_arns = [
+    aws_iam_policy.eks_admin.arn,
+  ]
 }
 
 # EKS Cluster
@@ -92,11 +90,9 @@ resource "aws_iam_role" "eks_cluster" {
     }]
     Version = "2012-10-17"
   })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cluster" {
-  role       = aws_iam_role.eks_cluster.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
+  ]
 }
 
 # EKS Node Group
@@ -113,19 +109,9 @@ resource "aws_iam_role" "eks_node_group" {
     }]
     Version = "2012-10-17"
   })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_worker_node" {
-  role       = aws_iam_role.eks_node_group.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "container_registry_read_only" {
-  role       = aws_iam_role.eks_node_group.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cni" {
-  role       = aws_iam_role.eks_node_group.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+  ]
 }

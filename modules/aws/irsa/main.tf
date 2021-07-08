f8286ea2624118ef6e49c23e0a658d7240cd4232
Philip Laine <philip.laine@xenit.se>
2021-07-08 14:52:16 +0200

    Add module to create irsa IAM roles

---
+++ modules/aws/irsa/main.tf
@@ -0,0 +1,63 @@
+/**
+  * # IRSA
+  *
+  * Creates IAM roles configured to work with EKS IRSA.
+  * Configures the important trust polcies to allow Kubernetes Service Accounts
+  * to assume the specific role.
+  */
+
+terraform {
+  required_version = "0.15.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.48.0"
+    }
+  }
+}
+
+data "aws_iam_policy_document" "assume" {
+  dynamic "statement" {
+    for_each = {
+      for v in var.oidc_urls :
+      v => v
+    }
+
+    content {
+      actions = [
+        "sts:AssumeRoleWithWebIdentity"
+      ]
+      effect = "Allow"
+
+      principals {
+        identifiers = [
+          each.value
+        ]
+        type = "Federated"
+      }
+
+      condition {
+        test     = "StringEquals"
+        variable = "${replace(each.value, "https://", "")}:sub"
+        values = [
+          "system:serviceaccount:${var.kubernetes_namespace}:${var.kubernetes_service_account}"
+        ]
+      }
+    }
+  }
+}
+
+resource "aws_iam_role" "this" {
+  name               = var.name
+  assume_role_policy = data.aws_iam_policy_document.assume.json
+}
+
+resource "aws_iam_policy" "permissions" {
+  name   = var.name
+  policy = var.policy_json
+}
+
+resource "aws_iam_role_policy_attachment" "permissions" {
+  policy_arn = aws_iam_policy.permissions.arn
+  role       = aws_iam_role.this.name
+}
---
+++ modules/aws/irsa/variables.tf
@@ -0,0 +1,24 @@
+variable "name" {
+  description = "Name of created IAM role and policy"
+  type        = string
+}
+
+variable "oidc_urls" {
+  description = "EKS OIDC URLs to trust"
+  type        = list(string)
+}
+
+variable "kubernetes_namespace" {
+  description = "Name of Kubernetes Namespace to trust"
+  type        = string
+}
+
+variable "kubernetes_service_account" {
+  description = "Name of Kubernetes Service Account to trust"
+  type        = string
+}
+
+variable "policy_json" {
+  description = "Permissions to apply to the created role"
+  type        = string
+}

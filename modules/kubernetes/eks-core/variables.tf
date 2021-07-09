bf043f193588410ba4ee95b9b63caae0454c0cbb
Philip Laine <philip.laine@xenit.se>
2021-07-09 14:50:35 +0200

    Add missing variables

--- modules/kubernetes/eks-core/variables.tf
+++ modules/kubernetes/eks-core/variables.tf
@@ -3,6 +3,16 @@ variable "environment" {
   type        = string
 }
 
+variable "name" {
+  description = "The name to use for the deploy"
+  type        = string
+}
+
+variable "eks_name_suffix" {
+  description = "The suffix for the eks clusters"
+  type        = number
+}
+
 variable "namespaces" {
   description = "The namespaces that should be created in Kubernetes."
   type = list(

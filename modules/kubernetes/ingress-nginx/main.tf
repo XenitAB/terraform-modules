4d4450e5830a36785d96ecf3396bd0ccefd3d999
Philip Laine <philip.laine@xenit.se>
2021-07-10 23:06:54 +0200

    Add network policy to ingress controller

--- modules/kubernetes/ingress-nginx/main.tf
+++ modules/kubernetes/ingress-nginx/main.tf
@@ -29,6 +29,137 @@ resource "kubernetes_namespace" "this" {
   }
 }
 
+resource "kubernetes_network_policy" "deny_default" {
+  metadata {
+    name      = "deny-default"
+    namespace = kubernetes_namespace.this.metadata[0].name
+  }
+
+  spec {
+    pod_selector {
+      match_labels = {}
+    }
+
+    policy_types = ["Ingress", "Egress"]
+
+    ingress {
+      from {
+        pod_selector {}
+      }
+    }
+
+    egress {
+      to {
+        namespace_selector {}
+        pod_selector {
+          match_labels = {
+            k8s-app = "kube-dns"
+          }
+        }
+      }
+
+      ports {
+        port     = 53
+        protocol = "UDP"
+      }
+    }
+
+    egress {
+      to {
+        pod_selector {}
+      }
+    }
+  }
+}
+
+resource "kubernetes_network_policy" "allow_scraping" {
+  metadata {
+    name      = "allow-scraping"
+    namespace = kubernetes_namespace.this.metadata[0].name
+  }
+
+  spec {
+    pod_selector {
+      match_labels = {
+        "app.kubernetes.io/component" = "controller"
+        "app.kubernetes.io/instance" = "ingress-nginx"
+        "app.kubernetes.io/name" = "ingress-nginx"
+      }
+    }
+
+    policy_types = ["Ingress"]
+
+    ingress {
+      from {
+        namespace_selector {
+          match_labels = {
+            name = "prometheus"
+          }
+        }
+        pod_selector {
+          match_labels = {
+            app = "prometheus"
+            "app.kubernetes.io/name" = "prometheus"
+          }
+        }
+      }
+
+      ports {
+        port = "metrics"
+        protocol = "TCP"
+      }
+    }
+  }
+}
+
+resource "kubernetes_network_policy" "allow_ingress_traffic" {
+  metadata {
+    name      = "allow-ingress-traffic"
+    namespace = kubernetes_namespace.this.metadata[0].name
+  }
+
+  spec {
+    pod_selector {
+      match_labels = {
+        "app.kubernetes.io/component" = "controller"
+        "app.kubernetes.io/instance" = "ingress-nginx"
+        "app.kubernetes.io/name" = "ingress-nginx"
+      }
+    }
+
+    policy_types = ["Ingress", "Egress"]
+
+    ingress {
+      from {
+        ip_block {
+          cidr = "0.0.0.0/0"
+          except = [
+            "10.0.0.0/8",
+            "172.16.0.0/12",
+            "192.168.0.0/16",
+          ]
+        }
+      }
+
+      ports {
+        port = "443"
+        protocol = "TCP"
+      }
+
+      ports {
+        port = "80"
+        protocol = "TCP"
+      }
+    }
+
+    egress {
+      pod_selector {
+        match_labels = {}
+      }
+    }
+  }
+}
+
 resource "helm_release" "ingress_nginx" {
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"

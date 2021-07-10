fd1d6a45cc74b3f5f8be306846a479394d6155f6
Philip Laine <philip.laine@xenit.se>
2021-07-10 22:24:23 +0200

    Allow metrics to goldpinger

--- modules/kubernetes/goldpinger/main.tf
+++ modules/kubernetes/goldpinger/main.tf
@@ -72,6 +72,45 @@ resource "kubernetes_network_policy" "deny_default" {
   }
 }
 
+resource "kubernetes_network_policy" "allow_scraping" {
+  metadata {
+    name      = "allow-scraping"
+    namespace = kubernetes_namespace.this.metadata[0].name
+  }
+
+  spec {
+    pod_selector {
+      match_labels = {
+        "app.kubernetes.io/instance" = "goldpinger"
+        "app.kubernetes.io/name" = "goldpinger"
+      }
+    }
+
+    policy_types = ["Ingress"]
+
+    ingress {
+      from {
+        namespace_selector {
+          match_labels = {
+            name = "prometheus"
+          }
+        }
+        pod_selector {
+          match_labels = {
+            app = "prometheus"
+            "app.kubernetes.io/name" = "prometheus"
+          }
+        }
+      }
+
+      ports {
+        port = "8080"
+        protocol = "TCP"
+      }
+    }
+  }
+}
+
 # Official Helm repo is deprecated to using fork.
 # https://github.com/bloomberg/goldpinger/issues/93
 resource "helm_release" "goldpinger" {

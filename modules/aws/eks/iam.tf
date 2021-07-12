2ffda858fe55225fae5183a52344367276a58490
Philip Laine <philip.laine@xenit.se>
2021-07-12 14:37:47 +0200

    Fix IAM policy

--- modules/aws/eks/iam.tf
+++ modules/aws/eks/iam.tf
@@ -169,7 +169,7 @@ data "aws_iam_policy_document" "xenit" {
       "ssm:GetParameter",
       "ssm:GetParameters",
     ]
-    resources = ["arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/xenit-proxy-certificate"]
+    resources = ["arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/xenit-proxy-certificate-*"]
   }
 }
 

3ef216e5087e2a2ae2e39b4276e55e620ac8f8a5
NissesSenap <NissesSenap@users.noreply.github.com>
2021-07-19 08:12:57 +0000

    Update versions

--- modules/kubernetes/ingress-healthz/main.tf
+++ modules/kubernetes/ingress-healthz/main.tf
@@ -36,7 +36,7 @@ resource "helm_release" "ingress_healthz" {
   chart      = "nginx"
   name       = "ingress-healthz"
   namespace  = kubernetes_namespace.this.metadata[0].name
-  version    = "9.3.6"
+  version    = "9.3.8"
   values = [templatefile("${path.module}/templates/values.yaml.tpl", {
     environment     = var.environment
     dns_zone        = var.dns_zone

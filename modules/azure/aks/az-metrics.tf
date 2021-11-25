a9862e103bf8be51a6e7edea0b872edb66af6d6f
Edvin Norling <edvin.norling@xenit.se>
2021-11-25 11:08:51 +0100

    rg.this.name is needed...
    Forgotten providers/ in aks

--- modules/azure/aks/az-metrics.tf
+++ modules/azure/aks/az-metrics.tf
@@ -17,13 +17,13 @@ resource "azurerm_role_assignment" "az_metrics" {
 }
 
 resource "azurerm_role_assignment" "az_metrics_aks_reader" {
-  scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourcegroups/${data.azurerm_resource_group.this}/Microsoft.ContainerService/managedClusters/aks-${var.environment}-${var.location_short}-${var.name}${var.aks_name_suffix}"
+  scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourcegroups/${data.azurerm_resource_group.this.name}/providers/Microsoft.ContainerService/managedClusters/aks-${var.environment}-${var.location_short}-${var.name}${var.aks_name_suffix}"
   role_definition_name = "Monitoring Reader"
   principal_id         = azurerm_user_assigned_identity.az_metrics.principal_id
 }
 
 resource "azurerm_role_assignment" "az_metrics_lb_reader" {
-  scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/mc_${data.azurerm_resource_group.this}_aks-${var.environment}-${var.location_short}-${var.name}${var.aks_name_suffix}_${data.azurerm_resource_group.this.location}/providers/Microsoft.Network/loadBalancers/kubernetes"
+  scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/mc_${data.azurerm_resource_group.this.name}_aks-${var.environment}-${var.location_short}-${var.name}${var.aks_name_suffix}_${data.azurerm_resource_group.this.location}/providers/Microsoft.Network/loadBalancers/kubernetes"
   role_definition_name = "Monitoring Reader"
   principal_id         = azurerm_user_assigned_identity.az_metrics.principal_id
 }

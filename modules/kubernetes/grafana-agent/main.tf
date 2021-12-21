3e4ee768c0cf0c5fe74fe7bb3473d305e5c66bcd
Simon Gottschlag <simon.gottschlag@xenit.se>
2021-12-21 13:25:31 +0100

    kubernetes/grafana-agent: add helm chart and namespace

--- modules/kubernetes/grafana-agent/main.tf
+++ modules/kubernetes/grafana-agent/main.tf
@@ -12,3 +12,22 @@ terraform {
     }
   }
 }
+
+resource "kubernetes_namespace" "this" {
+  metadata {
+    labels = {
+      name                = "grafana-agent"
+      "xkf.xenit.io/kind" = "platform"
+    }
+    name = "grafana-agent"
+  }
+}
+
+resource "helm_release" "grafana_agent_operator" {
+  repository  = "https://grafana.github.io/helm-charts"
+  chart       = "grafana-agent-operator"
+  name        = "grafana-agent-operator"
+  namespace   = kubernetes_namespace.this.metadata[0].name
+  version     = "0.1.4"
+  max_history = 3
+}

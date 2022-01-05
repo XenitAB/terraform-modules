17e5cebd843604697413284559bedddf81136f59
Philip Laine <philip.laine@gmail.com>
2022-01-05 10:27:13 +0100

    Update external-dns Helm charts (#487)

--- modules/kubernetes/external-dns/main.tf
+++ modules/kubernetes/external-dns/main.tf
@@ -37,7 +37,7 @@ resource "helm_release" "external_dns" {
   chart       = "external-dns"
   name        = "external-dns"
   namespace   = kubernetes_namespace.this.metadata[0].name
-  version     = "5.4.8"
+  version     = "6.0.2"
   max_history = 3
   values = [templatefile("${path.module}/templates/values.yaml.tpl", {
     provider     = var.dns_provider,

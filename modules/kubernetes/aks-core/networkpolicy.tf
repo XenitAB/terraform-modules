9d40d2daaa086b37ef5c00e5faf2ee90e85c9daf
Edvin Norling <edvin.norling@xenit.se>
2022-01-20 13:32:49 +0100

    Add grafana-agent and fix some selectors

--- modules/kubernetes/aks-core/networkpolicy.tf
+++ modules/kubernetes/aks-core/networkpolicy.tf
@@ -1,4 +1,4 @@
-resource "kubernetes_network_policy" "allow_datadog" {
+resource "kubernetes_network_policy" "allow_egress_datadog" {
   for_each = {
     for ns in var.namespaces :
     ns.name => ns
@@ -26,11 +26,62 @@ resource "kubernetes_network_policy" "allow_datadog" {
             "app.kubernetes.io/name"     = "datadog-agent-deployment"
           }
         }
+        namespace_selector {
+          match_labels = {
+            "name" = "datadog"
+          }
+        }
       }
 
       ports {
         port     = "8126"
         protocol = "TCP"
+      }
+    }
+  }
+}
+
+resource "kubernetes_network_policy" "allow_egress_ingress_grafana_agent" {
+  for_each = {
+    for ns in var.namespaces :
+    ns.name => ns
+    if var.grafana_agent_enabled
+  }
+
+  metadata {
+    name      = "allow-ingress-egress-grafana-agent"
+    namespace = kubernetes_namespace.tenant[each.key].metadata[0].name
+
+    labels = {
+      "xkf.xenit.io/kind" = "tenant"
+    }
+  }
+
+  spec {
+    pod_selector {}
+    policy_types = ["Egress", "Ingress"]
+
+    egress {
+      to {
+        pod_selector {
+          match_labels = {
+            "name" = "grafana-agent-traces"
+          }
+        }
+        namespace_selector {
+          match_labels = {
+            "name" = "grafana-agent"
+          }
+        }
+      }
+    }
+    ingress {
+      from {
+        namespace_selector {
+          match_labels = {
+            "name" = "grafana-agent"
+          }
+        }
       }
     }
   }

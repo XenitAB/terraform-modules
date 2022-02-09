364b3e8daa8015834734d8a6615e7d799f660b26
Tobias Vuorenmaa <tobias.vuorenmaa@azr-lab.net>
2022-02-09 11:40:28 +0100

    azure/core: Add var.subnet_private_endpoints
    We will úse this variable to enable private link policies for specific subnets.
    If the subnet is not defined we will default it to false.

--- modules/azure/core/subnets.tf
+++ modules/azure/core/subnets.tf
@@ -10,8 +10,8 @@ resource "azurerm_subnet" "this" {
   virtual_network_name                           = azurerm_virtual_network.this.name
   address_prefixes                               = [each.value.subnet_cidr]
   service_endpoints                              = each.value.subnet_service_endpoints
-  enforce_private_link_endpoint_network_policies = false
-  enforce_private_link_service_network_policies  = false
+  enforce_private_link_endpoint_network_policies = local.subnet_private_endpoints[each.value.subnet_short_name]
+  enforce_private_link_service_network_policies  = local.subnet_private_endpoints[each.value.subnet_short_name]
 }
 
 resource "azurerm_subnet" "aks" {
@@ -26,6 +26,6 @@ resource "azurerm_subnet" "aks" {
   virtual_network_name                           = azurerm_virtual_network.this.name
   address_prefixes                               = [each.value.subnet_cidr]
   service_endpoints                              = each.value.subnet_service_endpoints
-  enforce_private_link_endpoint_network_policies = false
-  enforce_private_link_service_network_policies  = false
+  enforce_private_link_endpoint_network_policies = local.subnet_private_endpoints[each.value.subnet_short_name]
+  enforce_private_link_service_network_policies  = local.subnet_private_endpoints[each.value.subnet_short_name]
 }

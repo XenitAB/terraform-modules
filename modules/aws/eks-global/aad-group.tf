7cefde4eb00918c76f74d4c637741230e9e1943c
Simon Gottschlag <simon.gottschlag@xenit.se>
2022-03-07 22:59:32 +0100

    azuread_group.name has been renamed azuread_group.display_name

--- modules/aws/eks-global/aad-group.tf
+++ modules/aws/eks-global/aad-group.tf
@@ -1,9 +1,9 @@
 locals {
   aad_groups = {
-    view          = { for k, v in azuread_group.view : k => { id = v.id, name = v.name } }
-    edit          = { for k, v in azuread_group.edit : k => { id = v.id, name = v.name } }
-    cluster_admin = { id = azuread_group.cluster_admin.id, name = azuread_group.cluster_admin.name }
-    cluster_view  = { id = azuread_group.cluster_view.id, name = azuread_group.cluster_view.name }
+    view          = { for k, v in azuread_group.view : k => { id = v.id, name = v.display_name } }
+    edit          = { for k, v in azuread_group.edit : k => { id = v.id, name = v.display_name } }
+    cluster_admin = { id = azuread_group.cluster_admin.id, name = azuread_group.cluster_admin.display_name }
+    cluster_view  = { id = azuread_group.cluster_view.id, name = azuread_group.cluster_view.display_name }
   }
 }
 
--- modules/azure/aks-global/locals.tf
+++ modules/azure/aks-global/locals.tf
@@ -6,11 +6,11 @@ locals {
     k => { id = v.id, client_id = v.client_id }
   }
   aad_groups = {
-    view                 = { for k, v in azuread_group.view : k => { id = v.id, name = v.name } }
-    edit                 = { for k, v in azuread_group.edit : k => { id = v.id, name = v.name } }
-    cluster_admin        = { id = azuread_group.cluster_admin.id, name = azuread_group.cluster_admin.name }
-    cluster_view         = { id = azuread_group.cluster_view.id, name = azuread_group.cluster_view.name }
-    aks_managed_identity = { id = azuread_group.aks_managed_identity.id, name = azuread_group.aks_managed_identity.name }
+    view                 = { for k, v in azuread_group.view : k => { id = v.id, name = v.display_name } }
+    edit                 = { for k, v in azuread_group.edit : k => { id = v.id, name = v.display_name } }
+    cluster_admin        = { id = azuread_group.cluster_admin.id, name = azuread_group.cluster_admin.display_name }
+    cluster_view         = { id = azuread_group.cluster_view.id, name = azuread_group.cluster_view.display_name }
+    aks_managed_identity = { id = azuread_group.aks_managed_identity.id, name = azuread_group.aks_managed_identity.display_name }
   }
 
   key_vault_default_permissions = {

8a6376a23676a1167e933bb749dadd8b72942586
Carl Andersson <carl.andersson@xenit.se>
2022-03-07 13:09:19 +0100

    Add initial config

--- modules/kubernetes/fluxcd-v2/variables.tf
+++ modules/kubernetes/fluxcd-v2/variables.tf
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 variable "environment" {
   description = "Environment name of the cluster"
   type        = string
@@ -33,12 +34,66 @@ variable "fleet_infra" {
     proj = string
     repo = string
   })
+=======
+variable "azure_devops_pat" {
+  description = "PAT to authenticate with Azure DevOps"
+  type        = string
+  sensitive   = true
+  default     = "null"
+}
+
+variable "azure_devops_org" {
+  description = "Azure DevOps organization for bootstrap repository"
+  type        = string
+  default     = "null"
+}
+
+variable "azure_devops_proj" {
+  description = "Azure DevOps project for bootstrap repository"
+  type        = string
+  default     = "null"
+}
+
+variable "github_org" {
+  description = "Org of GitHub repositories"
+  type        = string
+  default     = "null"
+}
+
+variable "github_app_id" {
+  description = "ID of GitHub Application used by Git Auth Proxy"
+  type        = number
+  default     = "null"
+}
+
+variable "github_installation_id" {
+  description = "Installation ID of GitHub Application used by Git Auth Proxy"
+  type        = number
+  default     = "null"
+}
+
+variable "github_private_key" {
+  description = "Private Key for GitHub Application used by Git Auth Proxy"
+  type        = string
+  default     = "null"
+}
+
+variable "environment" {
+  description = "Environment name of the cluster"
+  type        = string
+}
+
+variable "cluster_id" {
+  description = "Unique identifier of the cluster across regions and instances."
+  type        = string
+>>>>>>> Add initial config
 }
 
 variable "namespaces" {
   description = "The namespaces to configure flux with"
   type = list(
     object({
+<<<<<<< HEAD
       name        = string
       create_crds = bool
       org         = string
@@ -47,3 +102,28 @@ variable "namespaces" {
     })
   )
 }
+=======
+      name = string
+      flux = object({
+        enabled     = bool
+        create_crds = bool
+        org         = string
+        proj        = string
+        repo        = string
+      })
+    })
+  )
+}
+
+variable "cluster_repo" {
+  description = "Name of cluster repository"
+  type        = string
+  default     = "fleet-infra"
+}
+
+variable "branch" {
+  description = "Branch to point source controller towards"
+  type        = string
+  default     = "main"
+}
+>>>>>>> Add initial config

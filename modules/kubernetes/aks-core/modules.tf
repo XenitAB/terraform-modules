0b5591fccc2f703d3ecf8c629952ae7a6217ef33
Carl Andersson <carl.andersson@xenit.se>
2022-03-09 11:42:09 +0100

    Initial change of config to use one module - fluxcd-v2

--- modules/kubernetes/aks-core/modules.tf
+++ modules/kubernetes/aks-core/modules.tf
@@ -122,6 +122,7 @@ module "fluxcd_v2" {
   environment       = var.environment
   cluster_id        = "${var.location_short}-${var.environment}-${var.name}${var.aks_name_suffix}"
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
   environment = var.environment
   cluster_id  = "${var.location_short}-${var.environment}-${var.name}${var.aks_name_suffix}"
@@ -161,6 +162,16 @@ module "fluxcd_v2" {
     org         = ns.flux.org
     proj        = ns.flux.proj
     repo        = ns.flux.repo
+=======
+  credentials = var.fluxcd_v2_config.credentials
+  fleet_infra = var.fluxcd_v2_config.fleet_infra
+  namespaces = [for ns in var.namespaces : ns => ns if ns.flux.enabled  {
+    name = ns.name
+    create_crds = ns.flux.create_crds
+    org = ns.flux.org
+    proj = ns.flux.proj
+    repo = ns.flux.repo
+>>>>>>> dfd9823... Initial change of config to use one module - fluxcd-v2
   }]
 >>>>>>> 115a386... Initial change of config to use one module - fluxcd-v2
 }

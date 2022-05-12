e88767e74f49ba920e5bb9e492d2fa1e9fd778f1
Edvin N <edvin.norling@xenit.se>
2022-05-12 14:07:47 +0200

    Breaking governance move regional delegate-se to global module (#671)
    * Move delegate-se to global module
    
    
    Co-authored-by: Carl Andersson <carl.andersson@xenit.se>

--- modules/azure/governance-global/delegate-se.tf
+++ modules/azure/governance-global/delegate-se.tf
@@ -3,3 +3,33 @@ resource "azuread_group" "service_endpoint_join" {
   prevent_duplicate_names = true
   security_enabled        = true
 }
+
+resource "azuread_group_member" "service_endpoint_join_spn" {
+  for_each = {
+    for rg in var.resource_group_configs :
+    rg.common_name => rg
+    if rg.delegate_service_endpoint == true
+  }
+  group_object_id  = azuread_group.service_endpoint_join.id
+  member_object_id = azuread_service_principal.aad_sp[each.key].id
+}
+
+resource "azuread_group_member" "service_endpoint_join_owner" {
+  for_each = {
+    for rg in var.resource_group_configs :
+    rg.common_name => rg
+    if rg.delegate_service_endpoint == true
+  }
+  group_object_id  = azuread_group.service_endpoint_join.id
+  member_object_id = azuread_group.rg_owner[each.key].id
+}
+
+resource "azuread_group_member" "service_endpoint_join_contributor" {
+  for_each = {
+    for rg in var.resource_group_configs :
+    rg.common_name => rg
+    if rg.delegate_service_endpoint == true
+  }
+  group_object_id  = azuread_group.service_endpoint_join.id
+  member_object_id = azuread_group.rg_contributor[each.key].id
+}

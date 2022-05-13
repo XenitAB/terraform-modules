dbede7a5df2d2e0a25fd7092f09bccb83b698dd7
Carl Andersson <carl.andersson@xenit.se>
2022-05-13 14:54:42 +0200

    Move regions specific governance aad-groups

--- modules/azure/governance-regional/aad-group-rg.tf
+++ modules/azure/governance-regional/aad-group-rg.tf
@@ -1,4 +1,41 @@
 # AAD Group for Resource Group Owners
+resource "azuread_group" "rg_owner" {
+  for_each = {
+    for rg in var.resource_group_configs :
+    rg.common_name => rg
+  }
+
+  display_name            = "${var.azure_ad_group_prefix}-${var.location_short}-${var.subscription_name}-${var.environment}-${each.value.common_name}-owner"
+  prevent_duplicate_names = true
+  security_enabled        = true
+}
+
+# AAD Group for Resource Group Contributors
+resource "azuread_group" "rg_contributor" {
+  for_each = {
+    for rg in var.resource_group_configs :
+    rg.common_name => rg
+  }
+
+  display_name            = "${var.azure_ad_group_prefix}-${var.location_short}-${var.subscription_name}-${var.environment}-${each.value.common_name}-contributor"
+  prevent_duplicate_names = true
+  security_enabled        = true
+}
+
+
+# AAD Group for Resource Group Readers
+resource "azuread_group" "rg_reader" {
+  for_each = {
+    for rg in var.resource_group_configs :
+    rg.common_name => rg
+  }
+
+  display_name            = "${var.azure_ad_group_prefix}-${var.location_short}-${var.subscription_name}-${var.environment}-${each.value.common_name}-reader"
+  prevent_duplicate_names = true
+  security_enabled        = true
+}
+
+# AAD Group for Resource Group Owners
 resource "azurerm_role_assignment" "rg_owner" {
   for_each = {
     for rg in var.resource_group_configs :

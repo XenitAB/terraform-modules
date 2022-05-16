823506d0f5eb944b6e71611ce9dd9a955fe673ce
Carl Andersson <carl.andersson@xenit.se>
2022-05-16 10:41:41 +0200

    Fix validation

--- modules/azure/aks-global/outputs.tf
+++ modules/azure/aks-global/outputs.tf
@@ -27,9 +27,9 @@ output "aks_managed_identity_group_id" {
 }
 output "azuread_group_view_id" {
   description = "The group id of azure ad group edit"
-  value       = azuread_group.view[each.key].id
+  value       = azuread_group.view.*.id
 }
 output "azuread_group_edit_id" {
   description = "The group id of azure ad group edit"
-  value       = azuread_group.edit[each.key].id
+  value       = azuread_group.edit.*.id
 }

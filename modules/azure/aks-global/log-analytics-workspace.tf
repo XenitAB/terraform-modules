6d58e699a732c6f430f3c8e72a7ddb5a358a047f
edvin norling <edvin.norling@gmail.com>
2022-05-30 17:47:12 +0200

    Add analytics solution

--- modules/azure/aks-global/log-analytics-workspace.tf
+++ modules/azure/aks-global/log-analytics-workspace.tf
@@ -5,3 +5,16 @@ resource "azurerm_log_analytics_workspace" "aks" {
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
+
+resource "azurerm_log_analytics_solution" "logs" {
+  solution_name         = "ContainerInsights"
+  location              = azurerm_log_analytics_workspace.aks.location
+  resource_group_name   = data.azurerm_resource_group.this.name
+  workspace_resource_id = azurerm_log_analytics_workspace.aks.id
+  workspace_name        = azurerm_log_analytics_workspace.aks.name
+
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/ContainerInsights"
+  }
+}

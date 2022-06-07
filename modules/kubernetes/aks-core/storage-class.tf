1f11d551fb8246274b6bba30bb4916c15e2a5064
Philip Laine <philip.laine@gmail.com>
2022-06-07 09:27:30 +0200

    Add premium zrs storage class

--- modules/kubernetes/aks-core/storage-class.tf
+++ modules/kubernetes/aks-core/storage-class.tf
@@ -9,3 +9,15 @@ resource "kubernetes_storage_class" "zrs_standard" {
   }
   volume_binding_mode = "WaitForFirstConsumer"
 }
+
+resource "kubernetes_storage_class" "zrs_premium" {
+  metadata {
+    name = "managed-csi-premium-zrs"
+  }
+  storage_provisioner = "disk.csi.azure.com"
+  reclaim_policy      = "Delete"
+  parameters = {
+    skuName = "Premium_ZRS"
+  }
+  volume_binding_mode = "WaitForFirstConsumer"
+}

a0679d05b4509aaac115bc282c1e289bad166991
Edvin N <edvin.norling@xenit.se>
2022-06-13 10:27:38 +0200

    AKS Set kubelet config pod_max_pid (#641)
    * For now ignore kubelet_config on default_node_pool
    * Max pid to 1000

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 
 ## Unreleased
 
+### Changed
+
+- [#640](https://github.com/XenitAB/terraform-modules/pull/640) [Breaking] AKS set kubelet config default max pod pid to 1000.
+
 ## 2022.06.1
 
 ### Fix
--- modules/azure/aks/aks.tf
+++ modules/azure/aks/aks.tf
@@ -87,6 +87,10 @@ resource "azurerm_kubernetes_cluster_node_pool" "this" {
   node_taints = each.value.node_taints
   node_labels = merge({ "xkf.xenit.io/node-ttl" = "168h" }, each.value.node_labels, { "node-pool" = each.value.name })
 
+  kubelet_config {
+    pod_max_pid = 1000
+  }
+
   upgrade_settings {
     max_surge = "33%"
   }

7ab771ca8b473f934496c87e0aa63729b63eaadb
Jim Gustavsson <jim.a.gustavsson@gmail.com>
2022-06-21 11:38:59 +0200

    Add xks-global-data

---
+++ modules/azure/xks-global-data/outputs.tf
@@ -0,0 +1,10 @@
+output "aad_groups" {
+  description = "Azure AD groups"
+  value = {
+    view                 = { for k, v in data.azuread_group.view : k => { id = v.id, name = v.display_name } }
+    edit                 = { for k, v in data.azuread_group.edit : k => { id = v.id, name = v.display_name } }
+    cluster_admin        = { id = data.azuread_group.cluster_admin.id, name = data.azuread_group.cluster_admin.display_name }
+    cluster_view         = { id = data.azuread_group.cluster_view.id, name = data.azuread_group.cluster_view.display_name }
+    aks_managed_identity = var.cloud_provider == "azure" ? { id = data.azuread_group.aks_managed_identity["aks"].id, name = data.azuread_group.aks_managed_identity["aks"].display_name } : null
+  }
+}

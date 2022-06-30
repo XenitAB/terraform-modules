25bad4d18c2e39bd3b9b4fb504585e31f0d382ba
Edvin N <edvin.norling@xenit.se>
2022-06-30 10:36:22 +0200

    override public ip prefix name in aks global (#729)

--- modules/azure/aks-global/outbound-ips.tf
+++ modules/azure/aks-global/outbound-ips.tf
@@ -1,7 +1,7 @@
 resource "azurerm_public_ip_prefix" "aks" {
   count = var.public_ip_prefix_configuration.count
 
-  name                = "pip-prefix-${var.environment}-${var.location_short}-${var.name}-aks-${count.index}"
+  name                = "${local.aks_public_ip_preifx_name}-${count.index}"
   resource_group_name = data.azurerm_resource_group.this.name
   location            = data.azurerm_resource_group.this.location
   prefix_length       = var.public_ip_prefix_configuration.prefix_length

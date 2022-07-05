c0f44a940bf7d91f3c11bb723ce9278710e32f2c
Edvin Norling <edvin.norling@xenit.se>
2022-07-05 11:43:31 +0200

    Update description

--- modules/azure/governance-regional/variables.tf
+++ modules/azure/governance-regional/variables.tf
@@ -33,7 +33,7 @@ variable "resource_group_configs" {
       delegate_service_endpoint  = bool # Delegate Service Endpoint permissions
       delegate_service_principal = bool # Delegate Service Principal
       lock_resource_group        = bool # Adds management_lock (CanNotDelete) to the resource group
-      disable_unique_suffix      = bool # Disable unique_suffix in name of key vault
+      disable_unique_suffix      = bool # Disable unique_suffix on resource names
       tags                       = map(string)
     })
   )

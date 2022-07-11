e24dfb8a9e0e26d8e388b13b836e723b0dc96083
Philip Laine <philip.laine@gmail.com>
2022-07-11 13:02:31 +0200

    Make helm crd module template without validation

--- modules/kubernetes/helm-crd/main.tf
+++ modules/kubernetes/helm-crd/main.tf
@@ -20,6 +20,9 @@ data "helm_template" "this" {
   version      = var.chart_version
   include_crds = true
   api_versions = ["apiextensions.k8s.io/v1/CustomResourceDefinition"]
+  # Validate makes Helm run in client only mode. Fixes broken Kubernetes API version validation.
+  # https://github.com/hashicorp/terraform-provider-helm/blob/362ef45c3bc4eb8b7451396a2b8712d745281bc6/helm/data_template.go#L455
+  validate = true
 
   dynamic "set" {
     for_each = var.values

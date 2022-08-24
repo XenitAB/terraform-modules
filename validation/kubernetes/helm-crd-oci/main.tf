eb000712a475e5e5148838e03ab6b2bb918ee2cb
Edvin Norling <edvin.norling@xenit.se>
2022-08-24 15:36:44 +0200

    fmt

--- validation/kubernetes/helm-crd-oci/main.tf
+++ validation/kubernetes/helm-crd-oci/main.tf
@@ -3,6 +3,6 @@ terraform {}
 module "helm_crd" {
   source = "../../../modules/kubernetes/helm-crd-oci"
 
-  chart_name       = "bar"
-  chart_version    = "baz"
+  chart_name    = "bar"
+  chart_version = "baz"
 }

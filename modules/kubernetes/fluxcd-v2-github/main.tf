f25645fe1897bee7c44cd2006e45606df8693363
Edvin Norling <edvin.norling@xenit.se>
2022-09-02 11:06:26 +0200

    Upgrade flux v2 to 0.17.0 and update source CR

--- modules/kubernetes/fluxcd-v2-github/main.tf
+++ modules/kubernetes/fluxcd-v2-github/main.tf
@@ -25,7 +25,7 @@ terraform {
     }
     flux = {
       source  = "fluxcd/flux"
-      version = "0.11.2"
+      version = "0.17.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"

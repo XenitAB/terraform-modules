c16fef60f834ae22ce5f7a36e1d5edf730269bbe
Edvin N <edvin.norling@xenit.se>
2022-09-05 11:11:05 +0200

    upgrade AWS calico to 3.24 (#778)

--- modules/aws/eks/eks.tf
+++ modules/aws/eks/eks.tf
@@ -1,5 +1,5 @@
 locals {
-  calico_version = "v3.19"
+  calico_version = "v3.24"
   cni_script = templatefile("${path.module}/templates/update-eks-cni.sh.tpl", {
     b64_cluster_ca = aws_eks_cluster.this.certificate_authority[0].data,
     api_server_url = aws_eks_cluster.this.endpoint

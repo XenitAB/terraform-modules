e5fdb65a81e413d044c89d47339f539e451ac324
Edvin N <edvin.norling@xenit.se>
2022-09-05 11:32:55 +0200

    Update Falco helm chart to 2.0.16 (#775)
    * Update falco helm chart to 2.0.16
    * Update falco yaml to match the new default values

--- modules/kubernetes/falco/main.tf
+++ modules/kubernetes/falco/main.tf
@@ -36,7 +36,7 @@ resource "helm_release" "falco" {
   chart       = "falco"
   name        = "falco"
   namespace   = kubernetes_namespace.this.metadata[0].name
-  version     = "1.17.4"
+  version     = "2.0.16"
   max_history = 3
   values = [templatefile("${path.module}/templates/falco-values.yaml.tpl", {
     provider = var.cloud_provider
@@ -48,7 +48,7 @@ resource "helm_release" "falco_exporter" {
   chart       = "falco-exporter"
   name        = "falco-exporter"
   namespace   = kubernetes_namespace.this.metadata[0].name
-  version     = "0.8.0"
+  version     = "0.8.2"
   max_history = 3
   values      = [templatefile("${path.module}/templates/falco-exporter-values.yaml.tpl", {})]
 }

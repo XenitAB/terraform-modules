b3b7acdb9130cf107cb2a25d4c33fd542301b0bf
Edvin Norling <edvin.norling@xenit.se>
2022-09-07 17:04:52 +0200

    remove the index

--- modules/azure/aks/outputs.tf
+++ modules/azure/aks/outputs.tf
@@ -2,9 +2,9 @@ output "kube_config" {
   description = "Kube config for the created AKS cluster"
   sensitive   = true
   value = {
-    host                   = azurerm_kubernetes_cluster.this.kube_config_raw.0.host
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.this.kube_admin_config_raw.0.client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.this.kube_admin_config_raw.0.client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_admin_config_raw.0.cluster_ca_certificate)
+    host                   = azurerm_kubernetes_cluster.this.kube_config_raw.host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.this.kube_admin_config_raw.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.this.kube_admin_config_raw.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_admin_config_raw.cluster_ca_certificate)
   }
 }

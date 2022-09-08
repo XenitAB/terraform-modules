ec54a924146242c41058e39cfe4f4def625621d1
Edvin Norling <edvin.norling@xenit.se>
2022-09-08 11:08:05 +0200

    you don't want to know

--- modules/azure/aks/outputs.tf
+++ modules/azure/aks/outputs.tf
@@ -1,10 +1,20 @@
+locals {
+  host       = yamldecode(azurerm_kubernetes_cluster.this.kube_config_raw)["clusters"][0].cluster.server
+  auth_data  = yamldecode(azurerm_kubernetes_cluster.this.kube_config_raw)["clusters"][0].cluster.certificate-authority-data
+  apiVersion = yamldecode(azurerm_kubernetes_cluster.this.kube_config_raw)["users"][0].user.exec.apiVersion
+  args       = yamldecode(azurerm_kubernetes_cluster.this.kube_config_raw)["users"][0].user.exec.args
+  command    = yamldecode(azurerm_kubernetes_cluster.this.kube_config_raw)["users"][0].user.exec.command
+}
+
+
 output "kube_config" {
   description = "Kube config for the created AKS cluster"
-  sensitive   = true
+  sensitive   = false
   value = {
-    host                   = azurerm_kubernetes_cluster.this.kube_config_raw.host
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.this.kube_admin_config_raw.client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.this.kube_admin_config_raw.client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_admin_config_raw.cluster_ca_certificate)
+    host                   = local.host
+    cluster_ca_certificate = base64decode(local.auth_data)
+    api_version            = local.apiVersion
+    args                   = local.args
+    command                = local.command
   }
 }

377e30aaf55c1ecf4121ed74085a5361f6116ca2
Philip Laine <philip.laine@gmail.com>
2023-04-06 11:42:57 +0200

    Update main.tf

--- modules/kubernetes/datadog/main.tf
+++ modules/kubernetes/datadog/main.tf
@@ -14,14 +14,148 @@ terraform {
 
   required_providers {
     kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.13.1"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "2.6.0"
+      source  = "xenitab/git"
+      version = "0.0.1"
     }
   }
+}
+   
+resource "git_repository_file" "kustomization" {
+ path = "clsuters/we-dev-aks1/datadog.yaml"
+ content = <<EOF
+apiVersion: kustomize.toolkit.fluxcd.io/v1beta1
+kind: Kustomization
+metadata:
+  name: datadog
+  namespace: flux-system
+spec:
+  interval: 5m
+  sourceRef:
+    kind: GitRepository
+    name: flux-system
+  path: ./platform/we-dev-aks1/datadog
+  prune: true
+  validation: client
+ EOF
+}
+   
+resource "git_repository_file" "this" {
+ path = "platform/we-dev-aks1/datadog/datadog.yaml"
+ content = <<EOF
+apiVersion: v1
+kind: Namespace
+metadata:
+ name: datadog
+---
+apiVersion: source.toolkit.fluxcd.io/v1beta2
+kind: HelmRepository
+metadata:
+  name: datadog
+  namespace: datadog
+spec:
+  interval: 1m0s
+  url: "https://helm.datadoghq.com"
+---
+apiVersion: helm.toolkit.fluxcd.io/v2beta1
+kind: HelmRelease
+metadata:
+  name: datadog-operator
+  namespace: datadog
+spec:
+  chart:
+    spec:
+      crds: CreateReplace
+      chart: datadog-operator
+      sourceRef:
+        kind: HelmRepository
+        name: datadog
+      version: 0.8.0
+    values:
+      appKey: ${var.app_key}
+      apiKey: ${var.api_key}
+      installCRDs: false
+      datadogMonitor:
+        enabled: true
+      resources:
+        requests:
+          cpu: 15m
+          memory: 50Mi
+  interval: 1m0s
+---
+apiVersion: datadoghq.com/v1alpha1
+kind: DatadogAgent
+metadata:
+  name: datadog
+  namespace: datadog
+spec:
+  clusterName: {{ .Values.clusterName }}
+  site: {{ .Values.site }}
+  credentials:
+    apiSecret:
+      secretName: datadog-operator-apikey
+      keyName: api-key
+    appSecret:
+      secretName: datadog-operator-appkey
+      keyName: app-key
+  agent:
+    priorityClassName: platform-high
+    image:
+      name: "gcr.io/datadoghq/agent:latest"
+    log:
+      enabled: true
+      logsConfigContainerCollectAll: true
+    apm:
+      enabled: true
+      hostPort: 8126
+    env:
+      - name: DD_CONTAINER_EXCLUDE_LOGS
+        value: "name:datadog-agent"
+      - name: DD_CONTAINER_INCLUDE
+        value: {{ .Values.containerInclude }}
+      - name: DD_CONTAINER_EXCLUDE
+        value: "kube_namespace:.*"
+      - name: DD_APM_IGNORE_RESOURCES
+        value: {{ .Values.apmIgnoreResources }} 
+    config:
+      tolerations:
+        - operator: Exists
+      tags:
+        - env:{{ .Values.environment }}
+      kubelet:
+        tlsVerify: false
+      criSocket:
+        criSocketPath: /var/run/containerd/containerd.sock
+      volumeMounts:
+        - name: containerdsocket
+          mountPath: /var/run/containerd/containerd.sock
+      volumes:
+        - hostPath:
+            path: /var/run/containerd/containerd.sock
+          name: containerdsocket
+        - hostPath:
+            path: /var/run
+          name: var-run
+      resources:
+        requests:
+          cpu: 60m
+          memory: 200Mi
+  clusterAgent:
+    replicas: 2
+    priorityClassName: platform-low
+    config:
+      resources:
+        requests:
+          cpu: 60m
+          memory: 200Mi
+    image:
+      name: "gcr.io/datadoghq/cluster-agent:latest"
+  features:
+    kubeStateMetricsCore:
+      enabled: true
+    logCollection:
+      enabled: true
+      logsConfigContainerCollectAll: true
+ EOF
 }
 
 locals {

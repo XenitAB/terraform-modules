cf49e4fa01a4d34d07932367fe53d18aef678e7b
Carl Andersson <carl.andersson@xenit.se>
2023-04-19 11:22:19 +0200

    refactor to use if cloud_provider

--- modules/kubernetes/datadog/main.tf
+++ modules/kubernetes/datadog/main.tf
@@ -26,31 +26,53 @@ locals {
 }
 
 resource "git_repository_file" "kustomization" {
-  path = "clusters/${var.cluster_id}/datadog.yaml" # "clusters/we-dev-aks1/datadog.yaml"
+  path = "clusters/${var.cluster_id}/datadog.yaml"
   content = templatefile("${path.module}/templates/kustomization.yaml.tpl", {
     cluster_id = var.cluster_id
   })
 }
 
 resource "git_repository_file" "datadog-operator" {
-  path = "platform/${var.cluster_id}/datadog-operator/datadog-operator.yaml" # "platform/we-dev-aks1/datadog-operator.yaml"
-  content = templatefile("${path.module}/templates/datadog-operator.yaml.tpl", {
-    key_vault_name = var.azure_config.azure_key_vault_name
-    tenant_id      = var.azure_config.identity.tenant_id
-    resource_id    = var.azure_config.identity.resource_id
-    client_id      = var.azure_config.identity.client_id
-    cloud_provider = var.cloud_provider
-    role_arn       = var.aws_config.role_arn
-  })
+  path    = "platform/${var.cluster_id}/datadog-operator/datadog-operator.yaml"
+  content = "${path.module}/templates/datadog-operator.yaml"
 }
 
 resource "git_repository_file" "datadog" {
-  path = "platform/${var.cluster_id}/datadog/datadog.yaml" # "platform/we-dev-aks1/datadog.yaml"
+  path = "platform/${var.cluster_id}/datadog/datadog.yaml"
   content = templatefile("${path.module}/templates/datadog.yaml.tpl", {
     location             = var.location,
     environment          = var.environment,
     datadog_site         = var.datadog_site,
     namespace_include    = local.container_filter_include,
     apm_ignore_resources = local.apm_ignore_resources,
+  })
+}
+
+resource "git_repository_file" "azure_config" {
+  for_each = {
+    for s in ["azure-config"] :
+    s => s
+    if var.cloud_provider == "azure"
+  }
+
+  path = "platform/${var.cluster_id}/datadog-operator/azure-config.yaml"
+  content = templatefile("${path.module}/templates/azure-config.yaml.tpl", {
+    key_vault_name = var.azure_config.azure_key_vault_name
+    tenant_id      = var.azure_config.identity.tenant_id
+    resource_id    = var.azure_config.identity.resource_id
+    client_id      = var.azure_config.identity.client_id
+  })
+}
+
+resource "git_repository_file" "aws_config" {
+  for_each = {
+    for s in ["aws-config"] :
+    s => s
+    if var.cloud_provider == "aws"
+  }
+
+  path = "platform/${var.cluster_id}/datadog-operator/aws-config.yaml"
+  content = templatefile("${path.module}/templates/aws-config.yaml.tpl", {
+    role_arn = var.aws_config.role_arn
   })
 }

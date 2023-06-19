f5222a6f9bf89909f61f5305573481a147e31a55
Philip Laine <philip.laine@gmail.com>
2023-06-19 10:50:51 +0200

    Migrate node-ttl to install with Flux

--- modules/kubernetes/node-ttl/variables.tf
+++ modules/kubernetes/node-ttl/variables.tf
@@ -1,3 +1,8 @@
+variable "cluster_id" {
+  description = "Unique identifier of the cluster across regions and instances."
+  type        = string
+}
+
 variable "status_config_map_namespace" {
   description = "Namespace where Cluster Autoscaler status ConfigMap is created"
   type        = string

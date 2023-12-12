008b3e9f7a19a8ba608623f397f0849ae0e76a0a
lars.andersson@xenit.se <lars.andersson@xenit.se>
2023-12-12 09:25:58 +0100

    Add azad kube proxy password in core key vault

--- modules/azure/aks-regional/azad-kube-proxy.tf
+++ modules/azure/aks-regional/azad-kube-proxy.tf
@@ -10,3 +10,9 @@ module "azad_kube_proxy" {
   display_name = local.azad_kube_proxy_name
   cluster_name = local.azad_kube_proxy_name
 }
+
+resource "azurerm_key_vault_secret" "azad_kube_proxy" {
+  name         = "azad-kube-proxy-${var.environment}-${var.location_short}-${var.name}"
+  key_vault_id = data.azurerm_key_vault.core.id
+  value        = module.azad_kube_proxy.data.client_secret
+}

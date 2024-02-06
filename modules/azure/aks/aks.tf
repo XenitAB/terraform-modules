dd388f692f4b30d04b3949487e1f51e60657afd0
Carl Andersson <carl.andersson@xenit.se>
2024-02-06 16:33:49 +0100

    test

--- modules/azure/aks/aks.tf
+++ modules/azure/aks/aks.tf
@@ -218,8 +218,8 @@ resource "azurerm_role_assignment" "aks_managed_identity_noderg_virtual_machine_
   principal_id         = var.aad_groups.aks_managed_identity.id
 }
 
-# resource "azurerm_kubernetes_cluster_extension" "kv" {
-#   name           = "azure-keyvault-secrets-provider"
-#   cluster_id     = azurerm_kubernetes_cluster.this.id
-#   extension_type = "azure-keyvault-secrets-provider"
-# }
+resource "azurerm_kubernetes_cluster_extension" "kv" {
+  name           = "azure-keyvault-secrets-provider"
+  cluster_id     = azurerm_kubernetes_cluster.this.id
+  extension_type = "azure-keyvault-secrets-provider"
+}

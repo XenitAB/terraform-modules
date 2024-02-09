907adf7e3f0a1ba6f37b8eeeaab992c6ddc84026
Carl Andersson <91601778+CalleB3@users.noreply.github.com>
2024-02-09 09:18:17 +0100

    Make inlude_tenant_name optional (#1063)
    * try optional
    
    * changelog docs

--- modules/kubernetes/aks-core/variables.tf
+++ modules/kubernetes/aks-core/variables.tf
@@ -80,7 +80,7 @@ variable "namespaces" {
       flux = object({
         enabled             = bool
         create_crds         = bool
-        include_tenant_name = string
+        include_tenant_name = optional(bool, false)
         azure_devops = object({
           org  = string
           proj = string
@@ -96,9 +96,8 @@ variable "namespaces" {
     name   = ""
     labels = {}
     flux = {
-      enabled             = true
-      create_crds         = false
-      include_tenant_name = false
+      enabled     = true
+      create_crds = false
       azure_devops = {
         org  = ""
         proj = ""

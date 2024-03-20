8750edd017d158835fb7acef1b51bb4bcf4c20dc
lars.andersson@xenit.se <lars.andersson@xenit.se>
2024-03-20 16:04:37 +0100

    feature: Add support for creating additional storage classes

--- modules/kubernetes/aks-core/variables.tf
+++ modules/kubernetes/aks-core/variables.tf
@@ -654,3 +654,15 @@ variable "acr_name_override" {
   type        = string
   default     = ""
 }
+
+variable "additional_storage_classes" {
+  description = "List of additional storage classes to create"
+  type        = list(object({
+    name           = string
+    provisioner    = string
+    reclaim_policy = string
+    binding_mode   = string
+    sku_name       = string
+  }))
+  default     = []
+}

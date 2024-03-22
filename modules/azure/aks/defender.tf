b0750ed53b894c13a1f68d51409eadcbb1379bb6
lars.andersson@xenit.se <lars.andersson@xenit.se>
2024-03-22 13:06:55 +0100

    feat: Defender for containers

--- modules/azure/aks/defender.tf
+++ modules/azure/aks/defender.tf
@@ -74,7 +74,7 @@ resource "azurerm_resource_policy_assignment" "vulnerability_assessments" {
     {
       "effect": {
         "value": ${jsonencode((var.defender_enabled && var.defender_config.vulnerability_assessments_enabled) ? local.policy_effect_deploy : local.policy_effect_disable)}
-      }
+      },
       "isAgentlessDiscoveryForKubernetesEnabled": {
         "value": ${jsonencode(var.defender_enabled && var.defender_config.vulnerability_assessments_enabled ? true : false)}
       }
@@ -94,7 +94,7 @@ resource "azurerm_resource_policy_assignment" "agentless_discovery" {
     {
       "effect": {
         "value": ${jsonencode((var.defender_enabled && var.defender_config.kubernetes_discovery_enabled) ? local.policy_effect_deploy : local.policy_effect_disable)}
-      }
+      },
       "isContainerRegistriesVulnerabilityAssessmentsEnabled": {
         "value": ${jsonencode(var.defender_enabled && var.defender_config.kubernetes_discovery_enabled ? true : false)}
       }

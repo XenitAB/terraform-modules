8fa237288090b4ce1fdf0efff530391878ff23fc
lars.andersson@xenit.se <lars.andersson@xenit.se>
2024-04-05 13:33:07 +0200

    Fix build checks issues

--- validation/kubernetes/telepresence/main.tf
+++ validation/kubernetes/telepresence/main.tf
@@ -3,7 +3,7 @@ terraform {}
 module "telepresence" {
   source = "../../../modules/kubernetes/telepresence"
 
-  cluster_id          = "we-dev-aks"
+  cluster_id = "we-dev-aks"
   telepresence_config = {
     allow_conflicting_subnets = []
     client_rbac = {

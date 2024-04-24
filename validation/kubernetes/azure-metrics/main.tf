af1a6723058daa5c85e883c7579c3eb4ab826e97
Lars Andersson <lars.andersson@xenit.se>
2024-04-24 20:50:45 +0200

    Fix validation

--- validation/kubernetes/azure-metrics/main.tf
+++ validation/kubernetes/azure-metrics/main.tf
@@ -1,14 +1,9 @@
 terraform {}
 
-provider "kubernetes" {}
-
-provider "helm" {}
-
 module "azure_metrics" {
   source = "../../../modules/kubernetes/azure-metrics"
 
   client_id       = "1234"
-  resource_id     = "5678"
   subscription_id = "0987"
   cluster_id      = "foo"
 }

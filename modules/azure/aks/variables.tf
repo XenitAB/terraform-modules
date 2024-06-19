4021f078f7ec5ad737066c519bf52556818787eb
JH <jimmy.hagglund@xenit.se>
2024-06-19 09:24:24 +0200

    update of the default log retention time for azure to 30 days

--- modules/azure/aks/variables.tf
+++ modules/azure/aks/variables.tf
@@ -263,7 +263,7 @@ variable "namespaces" {
 variable "aks_audit_log_retention" {
   description = "The aks audit log retention in days, 0 = infinite"
   type        = number
-  default     = 365
+  default     = 30
 }
 
 variable "log_eventhub_name" {

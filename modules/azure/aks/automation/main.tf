733ee1141edcaa02b5072511373869bb1d9b55cd
Lars Andersson <lars.andersson@xenit.se>
2024-07-11 10:57:46 +0200

    fix: rename runkbook

--- modules/azure/aks/automation/main.tf
+++ modules/azure/aks/automation/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_automation_account" "aks" {
 }
 
 resource "azurerm_automation_runbook" "aks" {
-  name                    = "AKS-StartStop"
+  name                    = "AKS-StartStopScale"
   location                = data.azurerm_resource_group.this.location
   resource_group_name     = data.azurerm_resource_group.this.name
   automation_account_name = azurerm_automation_account.aks.name

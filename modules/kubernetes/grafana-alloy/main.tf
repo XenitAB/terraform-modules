972a0a663864a1386b10be677fad4d6ecd91bc6e
Risto Markovski <risto.markovski@unbox.onmicrosoft.com>
2024-08-13 15:36:06 +0200

    commas

--- modules/kubernetes/grafana-alloy/main.tf
+++ modules/kubernetes/grafana-alloy/main.tf
@@ -40,7 +40,7 @@ resource "git_repository_file" "kustomization" {
 resource "git_repository_file" "grafana_alloy" {
   path = "platform/${var.cluster_id}/grafana-alloy/grafana-alloy-controller.yaml"
   content = templatefile("${path.module}/templates/grafana-alloy-controller.yaml.tpl", {
-    azure_config        = var.azure_config
+    azure_config        = var.azure_config,
     environment         = var.environment,
     client_id           = data.azurerm_user_assigned_identity.xenit.client_id,
     tenant_id           = data.azurerm_user_assigned_identity.xenit.tenant_id,

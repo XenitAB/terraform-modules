0693e458abe29c435aa8600d6a21be33a87fc0a0
Carl Andersson <carl.andersson@xenit.se>
2024-10-04 14:50:50 +0200

    docs and validation

--- modules/kubernetes/azad-kube-proxy/main.tf
+++ modules/kubernetes/azad-kube-proxy/main.tf
@@ -25,17 +25,13 @@ terraform {
   required_version = ">= 1.3.0"
 
   required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.23.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "2.11.0"
-    }
     git = {
       source  = "xenitab/git"
       version = "0.0.3"
+    }
+    azurerm = {
+      version = "3.107.0"
+      source  = "hashicorp/azurerm"
     }
   }
 }
--- validation/kubernetes/azad-kube-proxy/main.tf
+++ validation/kubernetes/azad-kube-proxy/main.tf
@@ -12,15 +12,15 @@ module "azad_kube_proxy" {
     helm       = helm
   }
 
-  fqdn       = "k8s.example.com"
-  cluster_id = "test"
-  environment = "test"
-  location = "test"
-  location_short = "we"
+  fqdn                = "k8s.example.com"
+  cluster_id          = "test"
+  environment         = "test"
+  location            = "test"
+  location_short      = "we"
   resource_group_name = "test"
-  dns_zones = ["test"]
-  name = "core"
-  key_vault_id = "foo"
-  key_vault_name = "foo"
-  oidc_issuer_url = "test.com"
+  dns_zones           = ["test"]
+  name                = "core"
+  key_vault_id        = "foo"
+  key_vault_name      = "foo"
+  oidc_issuer_url     = "test.com"
 }

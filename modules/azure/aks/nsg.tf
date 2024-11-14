2ba6938d8e6c1476ef8b24e4955371c5fa8fdf64
Carl Andersson <carl.andersson@xenit.se>
2024-11-14 11:11:17 +0100

    Add tolist on ips

--- modules/azure/aks/nsg.tf
+++ modules/azure/aks/nsg.tf
@@ -1,9 +1,8 @@
-data "azurerm_resources" "public_ips" {
+data "azurerm_resources" "this" {
   count               = var.add_default_security_lb_rule ? 1 : 0
   resource_group_name = azurerm_kubernetes_cluster.this.node_resource_group
   type                = "Microsoft.Network/publicIPAddresses"
 }
-
 
 resource "azurerm_network_security_rule" "allow_internet_azure_lb" {
   count                       = var.add_default_security_lb_rule ? 1 : 0
@@ -15,7 +14,7 @@ resource "azurerm_network_security_rule" "allow_internet_azure_lb" {
   source_port_range           = "*"
   destination_port_range      = "80,443"
   source_address_prefix       = "Internet"
-  destination_address_prefix  = data.azurerm_resources.public_ips
+  destination_address_prefix = data.azurerm_resources.this[count.index]
   resource_group_name         = data.azurerm_resource_group.this.name
   network_security_group_name = "nsg-${var.environment}-${var.location_short}-${var.core_name}-${var.name}${var.aks_name_suffix}"
 }

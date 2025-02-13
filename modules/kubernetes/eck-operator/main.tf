05fdd841ee6f195400d4e92eaaa76a8ee6ba77a1
optocoupler <risto.markovski@xenit.se>
2025-02-13 14:22:17 +0100

    another

--- modules/kubernetes/eck-operator/main.tf
+++ modules/kubernetes/eck-operator/main.tf
@@ -34,7 +34,7 @@ resource "kubernetes_namespace" "this" {
 }
 
 resource "git_repository_file" "kustomization" {
-  path       = "clusters/${var.cluster_id}/eck-operator.yaml.yaml"
+  path       = "clusters/${var.cluster_id}/eck-operator.yaml"
   depends_on = [kubernetes_namespace.this]
   content = templatefile("${path.module}/templates/kustomization.yaml.tpl", {
     cluster_id = var.cluster_id,

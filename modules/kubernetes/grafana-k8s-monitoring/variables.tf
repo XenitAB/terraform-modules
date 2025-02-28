03de1a3048843ad1b44a449282bff0f9053ff9a7
optocoupler <risto.markovski@xenit.se>
2025-02-28 10:22:24 +0100

    chore: consolidating all monitoring resources to avoid CRD issues (#1311)
    * chore: consolidating all monitoring resources to avoid CRD race condition
    
    * removing unused var

--- modules/kubernetes/grafana-k8s-monitoring/variables.tf
+++ modules/kubernetes/grafana-k8s-monitoring/variables.tf
@@ -43,3 +43,83 @@ variable "location" {
   description = "The Azure region name."
   type        = string
 }
+
+# Opt In Monitors
+
+
+variable "falco_enabled" {
+  description = "Should Falco be enabled"
+  type        = bool
+  default     = false
+}
+
+variable "gatekeeper_enabled" {
+  description = "Should OPA Gatekeeper be enabled"
+  type        = bool
+  default     = false
+}
+
+variable "linkerd_enabled" {
+  description = "Should linkerd be enabled"
+  type        = bool
+  default     = false
+}
+
+variable "flux_enabled" {
+  description = "Should flux-system be enabled"
+  type        = bool
+  default     = false
+}
+
+variable "aad_pod_identity_enabled" {
+  description = "Should aad pod dentity be enabled"
+  type        = bool
+  default     = false
+}
+
+variable "azad_kube_proxy_enabled" {
+  description = "Should azad-kube-proxy be enabled"
+  type        = bool
+  default     = false
+}
+
+variable "grafana_agent_enabled" {
+  description = "Should grafana-agent be enabled"
+  type        = bool
+  default     = false
+}
+
+variable "trivy_enabled" {
+  description = "Should trivy be enabled"
+  type        = bool
+  default     = false
+}
+
+variable "node_local_dns_enabled" {
+  description = "Should node local DNS be enabled"
+  type        = bool
+  default     = false
+}
+
+variable "promtail_enabled" {
+  description = "Should promtail be enabled"
+  type        = bool
+  default     = false
+}
+
+variable "node_ttl_enabled" {
+  description = "Should Node TTL be enabled"
+  type        = bool
+  default     = false
+}
+
+variable "spegel_enabled" {
+  description = "Should Spegel be enabled"
+  type        = bool
+  default     = false
+}
+variable "cilium_enabled" {
+  description = "If enabled, will use Azure CNI with Cilium instead of kubenet"
+  type        = bool
+  default     = false
+}

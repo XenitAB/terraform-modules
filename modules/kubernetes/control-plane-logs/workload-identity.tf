20674904b2e05ed98522afcf027b2a87a1ac9876
Lars Andersson <lars.andersson@xenit.se>
2025-04-07 11:07:23 +0200

    fix: federated identity credentials for vector

--- modules/kubernetes/control-plane-logs/workload-identity.tf
+++ modules/kubernetes/control-plane-logs/workload-identity.tf
@@ -9,5 +9,5 @@ resource "azurerm_federated_identity_credential" "control_plane_logs" {
   parent_id           = data.azurerm_user_assigned_identity.xenit.id
   audience            = ["api://AzureADTokenExchange"]
   issuer              = var.oidc_issuer_url
-  subject             = "system:serviceaccount:controle-plane-logs:vector"
+  subject             = "system:serviceaccount:control-plane-logs:vector"
 }

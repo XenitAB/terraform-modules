57a286332eb9f40f3a62c3bc2e65b296b8162a8c
Carl Andersson <carl.andersson@xenit.se>
2025-09-29 13:52:25 +0200

    remove git.tenant provider

--- modules/kubernetes/aks-core/main.tf
+++ modules/kubernetes/aks-core/main.tf
@@ -16,11 +16,6 @@ terraform {
       version = "2.50.0"
       source  = "hashicorp/azuread"
     }
-    git = {
-      source                = "xenitab/git"
-      version               = ">=0.0.4"
-      configuration_aliases = [git.tenant]
-    }
     random = {
       source  = "hashicorp/random"
       version = "3.5.1"

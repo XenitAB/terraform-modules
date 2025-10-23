4e4bb9a6fc940ac5a4d09a8fd46b6f7d5d6b9e5f
Carl Andersson <carl.andersson@xenit.se>
2025-10-23 10:15:02 +0200

    try fixing adding of namespaces again

--- modules/kubernetes/azure-policy/locals.tf
+++ modules/kubernetes/azure-policy/locals.tf
@@ -2,7 +2,7 @@ locals {
   system_namespaces = "'calico-system', 'gatekeeper-system', 'kube-system', 'tigera-operator'"
 
   exclude_namespaces = join(", ", [
-    for ns in var.azure_policy_config.exclude_namespaces : "\"${ns}\""
+    for ns in var.azure_policy_config.exclude_namespaces : "'${ns}'"
   ])
 
   azure_identity_format = base64encode(

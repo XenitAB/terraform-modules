25775091285d5fb812a3cfd2c7d68adb171d8e92
Carl Andersson <91601778+CalleB3@users.noreply.github.com>
2026-01-30 16:36:23 +0100

    Update subject for workload identity in Terraform

--- modules/kubernetes/grafana-k8s-monitoring-lite/workload-identity.tf
+++ modules/kubernetes/grafana-k8s-monitoring-lite/workload-identity.tf
@@ -10,7 +10,7 @@ resource "azurerm_federated_identity_credential" "grafana_k8s_monitor_lite" {
   parent_id           = azurerm_user_assigned_identity.grafana_k8s_monitor_lite.id
   audience            = ["api://AzureADTokenExchange"]
   issuer              = var.oidc_issuer_url
-  subject             = "system:serviceaccount:grafana-k8s-monitoring-lite:grafana-k8s-monitoring-alloy-metrics"
+  subject             = "system:serviceaccount:grafana-k8s-monitoring-lite:grafana-k8s-monitoring-lite-alloy-metrics"
 }
 
 resource "azurerm_key_vault_access_policy" "grafana_k8s_monitor_lite" {

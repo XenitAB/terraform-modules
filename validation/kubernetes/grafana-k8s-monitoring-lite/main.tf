16c522a0a1084757a2bcc375ffb27d4286b03846
yabracadabra <jimmy.hagglund@xenit.se>
2026-01-30 14:30:14 +0100

    docs: update docs

---
+++ validation/kubernetes/grafana-k8s-monitoring-lite/main.tf
@@ -0,0 +1,31 @@
+ terraform {}
+
+provider "kubernetes" {}
+
+provider "helm" {}
+
+module "grafana-k8s-monitoring-lite" {
+  source              = "../../../modules/kubernetes/grafana-k8s-monitoring"
+  cluster_id          = "yabadabadoo"
+  cluster_name        = "foobar123"
+  key_vault_id        = "secretstuff"
+  resource_group_name = "foo"
+  location            = "narnia"
+  oidc_issuer_url     = "gimme safe auth"
+  grafana_k8s_monitor_config = {
+    grafana_cloud_prometheus_host = "barfoo"
+    grafana_cloud_loki_host       = "foobar"
+    grafana_cloud_tempo_host      = "bar123"
+    azure_key_vault_name          = "boobooyaba"
+    include_namespaces            = "one,two,three"
+    exclude_namespaces            = ["threetwoone"]
+  }
+  tenant_name = "foo"
+  environment = "dev"
+  fleet_infra_config = {
+    argocd_project_name = "foo-fleet-infra"
+    git_repo_url        = "http://some-git-repo.git"
+    k8s_api_server_url  = "http://kubernetes.default.svc"
+  }
+  subscription_id = "subscription-id"
+}

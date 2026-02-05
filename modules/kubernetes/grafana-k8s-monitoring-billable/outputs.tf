e75e17be511f6b84f2d35b525a69bdb2c3385f4c
yabracadabra <jimmy.hagglund@xenit.se>
2026-02-05 14:09:48 +0100

    fix: workload identity reference

--- modules/kubernetes/grafana-k8s-monitoring-billable/outputs.tf
+++ modules/kubernetes/grafana-k8s-monitoring-billable/outputs.tf
@@ -1,4 +1,4 @@
 output "client_id" {
   description = "Client ID of the managed identity"
-  value       = azurerm_user_assigned_identity.grafana_k8s_monitor_lite.client_id
+  value       = azurerm_user_assigned_identity.grafana_k8s_monitor_billable.client_id
 }

93b10ab5b97c7b5b8ea423c94a4e7045a5195505
forsmans <forsman.magnus@gmail.com>
2026-02-19 13:07:09 +0100

    rbac

--- modules/azure/governance-regional/locals.tf
+++ modules/azure/governance-regional/locals.tf
@@ -1,50 +1,13 @@
 locals {
-  key_vault_default_permissions = {
-    key_permissions = [
-      "Backup",
-      "Create",
-      "Decrypt",
-      "Delete",
-      "Encrypt",
-      "Get",
-      "Import",
-      "List",
-      "Purge",
-      "Recover",
-      "Restore",
-      "Sign",
-      "UnwrapKey",
-      "Update",
-      "Verify",
-      "WrapKey"
+  key_vault_rbac_roles = {
+    key_secret = [
+      "Key Vault Crypto Officer",
+      "Key Vault Secrets Officer"
     ]
-    secret_permissions = [
-      "Backup",
-      "Delete",
-      "Get",
-      "List",
-      "Purge",
-      "Recover",
-      "Restore",
-      "Set"
-    ]
-    certificate_permissions = [
-      "Backup",
-      "Create",
-      "Delete",
-      "DeleteIssuers",
-      "Get",
-      "GetIssuers",
-      "Import",
-      "List",
-      "ListIssuers",
-      "ManageContacts",
-      "ManageIssuers",
-      "Purge",
-      "Recover",
-      "Restore",
-      "SetIssuers",
-      "Update"
+    key_secret_cert = [
+      "Key Vault Crypto Officer",
+      "Key Vault Secrets Officer",
+      "Key Vault Certificates Officer"
     ]
   }
 }

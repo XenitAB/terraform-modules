6ff3462ce7653a21b4d16dc126e7d72c79085604
yabracadabra <jimmy.hagglund@xenit.se>
2026-03-19 15:18:31 +0100

    fix: removal of sysctl.vm_max_map_count

--- modules/azure/aks/aks.tf
+++ modules/azure/aks/aks.tf
@@ -185,8 +185,8 @@ resource "azurerm_kubernetes_cluster_node_pool" "this" {
   eviction_policy             = each.value.spot_enabled ? "Delete" : null
   priority                    = each.value.spot_enabled ? "Spot" : "Regular"
   spot_max_price              = each.value.spot_max_price
-  node_taints = each.value.node_taints
-  node_labels = merge({ "xkf.xenit.io/node-ttl" = "168h" }, each.value.node_labels, { "xkf.xenit.io/node-pool" = each.value.name })
+  node_taints                 = each.value.node_taints
+  node_labels                 = merge({ "xkf.xenit.io/node-ttl" = "168h" }, each.value.node_labels, { "xkf.xenit.io/node-pool" = each.value.name })
 
   kubelet_config {
     pod_max_pid           = 1000
